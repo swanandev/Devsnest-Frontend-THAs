c87ba429f60fb26a3a67bfc7795265ebedec42c9
Swanand Buva <swanandev@gmail.com>
2021-07-11 18:15:26 +0530

    Day 16 Deployment Fix!!

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ const createHrefElement = (day_no) => {
     anchor.innerHTML = "Day "+ day_no;
 }
 
-const dir = (location.hostname === "localhost" || location.hostname === "127.0.0.1" ? "/Day_" : "/Devsnest-Frontend-THAs/Day_")
+const localDirs = ['/Day_', '/day_']
+const publicDirs = ['/Devsnest-Frontend-THAs/Day_', '/Devsnest-Frontend-THAs/day_']
+
+const dir = (location.hostname === "localhost" || location.hostname === "127.0.0.1" ? localDirs : publicDirs)
 
 const day_nos = ['01_2', '03', '04', '05', '06', '07', '08', '09','10', '11', '12', '13'];
 
@@ -18,7 +21,7 @@ const react_day_nos = ['16', '17', '18', '19'];
 
 const count = day_nos.length + react_day_nos.length;
 
-const createTHADiv = (inx, day_no, url, ) => {
+const createTHADiv = (inx, day_no, url, dirIndex) => {
     let thaDiv = document.createElement('div');
     let text = document.createElement('p');
     text.innerHTML = "Day " + day_no;
@@ -26,16 +29,16 @@ const createTHADiv = (inx, day_no, url, ) => {
     thaDiv.setAttribute('id', (inx+1));
     thaDiv.addEventListener('click',() => {
         //pix.classList.toggle('booked');
-        window.location.href = dir + (url);
+        window.location.href = dir[dirIndex] + (url);
     });
     thaDiv.appendChild(text);
     return thaDiv;
 }
 
 day_nos.forEach( (day_no, inx) => {
-    thaGridClass.appendChild(createTHADiv(inx, day_no, day_no));
+    thaGridClass.appendChild(createTHADiv(inx, day_no, day_no, 0));
 })
 
 react_day_nos.forEach( (day_no, inx) => {
-    thaGridClass.appendChild(createTHADiv(day_nos.length + inx, day_no, day_no+"/build/"));
+    thaGridClass.appendChild(createTHADiv(day_nos.length + inx, day_no, day_no+"/build/", 1));
 })
