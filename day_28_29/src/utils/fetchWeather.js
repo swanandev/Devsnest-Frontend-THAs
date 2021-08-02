4ce9dbe70e8cb3ff17c2f5a8ba571cad95dcd828
Swanand Buva <swanandev@gmail.com>
2021-08-02 19:28:11 +0530

    Lil Fix!!

--- day_28_29/src/utils/fetchWeather.js
+++ day_28_29/src/utils/fetchWeather.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "http://api.weatherapi.com/v1/current.json?key=8e6a7cfb5049423db6d92748210108&q=";
+  "https://api.weatherapi.com/v1/current.json?key=8e6a7cfb5049423db6d92748210108&q=";
 
 export const fetchWeatherData = (place) => {
   return fetch(API_URL + place)
